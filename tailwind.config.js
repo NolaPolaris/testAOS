1cdfcad1be4d288886a2ae53596909ac9094117d
Julie <julietaubry@gmail.com>
2021-07-24 12:06:45 +0200

    test plugin ratio Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@ module.exports = {
   purge: ['./dist/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '64': '16rem',
+        '68': '17rem',
+        '72': '18rem',
+        '80': '20rem',
+        '88': '22rem',
+        '120': '30rem',
+        '160': '40rem',
+      },
+
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],  
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
