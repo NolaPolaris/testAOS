056140095a0a7a662227eb1cf27049f7b6d6e340
Julie <julietaubry@gmail.com>
2021-07-25 10:10:33 +0200

    cdn ok

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,4 @@
 import '../css/style.css';
 import './api.js'
-
-// import AOS from 'aos';
-// import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-
+import AOS from 'aos';
 AOS.init();
